8dc4c48e2764e78e8235730644de84e2b2a71648
Timo Scheuermann <timo.scheuermann@gmx.de>
2020-04-25 15:02:22 +0200

    added pwa icon v0.0.1

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,13 @@ module.exports = {
     themeColor: "#f0f0f0",
     msTileColor: "#111111",
     appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black-translucent"
+    appleMobileWebAppStatusBarStyle: "black-translucent",
+    iconPaths: {
+      favicon32: "img/icons/favicon-32x32.png",
+      favicon16: "img/icons/favicon-16x16.png",
+      appleTouchIcon: "img/icons/icon-152x152.png",
+      maskIcon: "img/icons/icon.svg",
+      msTileImage: "img/icons/icon-144x144.png"
+    }
   }
 };
